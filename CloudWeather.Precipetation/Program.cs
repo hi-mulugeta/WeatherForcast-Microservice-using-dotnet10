30a84ea6b41e5d995ee277c9830610a04bc4e367
Mulugeta Mehari <debebefekadu14@gmail.com>
2026-01-11 21:05:57 +0300

    feat: added packages to all 3 projects

--- CloudWeather.Precipetation/Program.cs
+++ CloudWeather.Precipetation/Program.cs
@@ -1,6 +1,15 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
-
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/observation/{zipCode}", (string zipCode,[FromQuery] int? days) =>
+{
+    // Placeholder for precipitation observation logic
+    return $"Let there be an eternal light {zipCode}";
+}); 
 app.Run();
